272335a66797efd6ebcf2e78495c109a640cdddc
Estuardo Diaz <taro@pixela.tech>
2021-11-10 16:19:44 -0600

    Adds initial assets

--- main.js
+++ main.js
@@ -1,6 +1,64 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// 1. En una variable llamada holes, guardar todos los elementos de tipo .hole
+const holes = document.querySelectorAll('.hole');
+const score = document.querySelector('.score-value');
+const bugs = document.querySelectorAll('.bug');
+
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+// Retorna un tiempo aleatorio entre un rango definido
+function tiempoRandom(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+// Retorna un aleatoriamente un elemento de tipo .hole
+// de una lista de agujeros enviada como parámetros
+function agujeroRandom(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+
+  if (lastHole === hole) {
+    return agujeroRandom(holes);
+  }
+  
+  lastHole = hole;
+  return hole
+}
+
+
+// Hace que un nuevo bug salte desde un agujero
+function saltar() {
+  const time = tiempoRandom(200, 1000);
+  const hole = agujeroRandom(holes);
+
+  // 2. Agregar la clase 'up' al elemento 'hole' para que el bug aparezca
+  
+  setTimeout(() => {
+    // 3. Eliminar la clase 'up' para que el bug desaparezca
+
+    // Hace que un nuevo bug salte desde un agujero si el tiempo no ha terminado
+    if (!timeUp) saltar();
+  }, time)
+}
+
+// Maneja el evento de click en un bug
+function golpear(event) {
+  if (!event.isTrusted) return; // Alguien intentó hacer trampa
+
+  score++;
+  this.classList.remove('up');
+  score.textContent = score;
+}
+
+function iniciarJuego() {
+  score.textContent = 0;
+  timeUp = false;
+
+  saltar();
+
+  setTimeout(() => timeUp = true, 10 * 1000);
+}
+
